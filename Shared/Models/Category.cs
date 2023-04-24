acbe754e0c297c3c0db1d836941b39afff43be34
Alvaro F <alvaro.fromero@gmail.com>
2023-04-24 09:06:13 +0200

    Category Model/Entity created

---
+++ Shared/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Models
+{
+	public class Category
+	{
+		[Key]
+		[Required]
+		public int CategoryId { get; set; }
+
+		[Required]
+		[MaxLength(256)] // words
+		public string ThumbnailImagePath { get; set; }
+
+		[Required]
+		[MaxLength(128)] // words
+		public string Name { get; set; }
+
+		[Required]
+		[MaxLength(1024)] // words
+		public string Description { get; set; }
+	}
+}
