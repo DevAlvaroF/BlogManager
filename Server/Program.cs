247f5ce58e3c086915221195ef64d42b5fa6b3a1
Alvaro F <alvaro.fromero@gmail.com>
2023-04-24 09:49:45 +0200

    Add static files usage and connection string on appSettings.json

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,22 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDBContext>(options =>
+{
+	options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "Server", Version = "v1" });
+});
+
+
+// ================================================
+// Convert all API Uri to lower case
+// ================================================
+
+builder.Services.AddRouting(options =>
+{
+	options.LowercaseUrls = true;
+});
+
+// Add services to the container.
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+	app.UseDeveloperExceptionPage();
 	app.UseSwagger();
-	app.UseSwaggerUI();
+	//app.UseSwaggerUI(c => c.SwaggerEndpoint);
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
