195f930e07acffc62043d426d180ed200f7ec0fe
Alvaro F <alvaro.fromero@gmail.com>
2023-04-24 09:22:02 +0200

    Created AppDBContext for SQLLite on the Categories Entity

---
+++ Server/Data/AppDBContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace Server.Data
+{
+	public class AppDBContext : DbContext
+	{
+		public DbSet<Category> Categories { get; set; }
+
+		public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			// Call the base version of the method
+			base.OnModelCreating(modelBuilder);
+
+			// ======================================
+			// Add seed Data
+			// ======================================
+			Category[] categoriesToSeed = new Category[3];
+
+			for (int i = 1; i < 4; i++)
+			{
+				categoriesToSeed[i - 1] = new Category()
+				{
+					CategoryId = i,
+					ThumbnailImagePath = "uploads/placeholder.jpg",
+					Name = $"Category {i}",
+					Description = $"A description of category {i}",
+				};
+			}
+
+			// If data does not exist, seed it
+			modelBuilder.Entity<Category>().HasData(categoriesToSeed);
+		}
+	}
+}
