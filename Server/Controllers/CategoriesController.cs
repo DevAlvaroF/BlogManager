25ea8455bf0865462839040bf3d9200fe41d35f2
Alvaro F <alvaro.fromero@gmail.com>
2023-04-24 15:56:08 +0200

    Expanding API to retrieve categories with newly added posts relation in the database

--- Server/Controllers/CategoriesController.cs
+++ Server/Controllers/CategoriesController.cs
@@ -24,5 +24,58 @@ namespace Server.Controllers
 			return Ok(categories);
 		}
 
+		[HttpGet("withposts")] // website.com/api/categories/withposts
+		public async Task<IActionResult> GetWithPosts()
+		{
+			// Return categories from database
+			List<Category> categories = await _appDBContext.Categories.Include(category => category.Posts).ToListAsync();
+
+			return Ok(categories);
+		}
+
+		[HttpGet("{id}")] // website.com/api/categories/3
+		public async Task<IActionResult> Get(int id)
+		{
+			Category category = await GetCategoryByCategoryId(id, false);
+
+			return Ok(category);
+		}
+
+		[HttpGet("withposts/{id}")] // website.com/api/categories/withposts/3
+		public async Task<IActionResult> GetWithPosts(int id)
+		{
+			Category category = await GetCategoryByCategoryId(id, true);
+
+			return Ok(category);
+		}
+		#region Utility Methods
+
+		[NonAction]
+		[ApiExplorerSettings(IgnoreApi = true)]
+		private async Task<bool> PersistChangesToDatabase()
+		{
+			int amountOfChanges = await _appDBContext.SaveChangesAsync();
+
+			return amountOfChanges > 0;
+		}
+
+		[NonAction]
+		[ApiExplorerSettings(IgnoreApi = true)]
+		private async Task<Category> GetCategoryByCategoryId(int categoryId, bool withPosts)
+		{
+			Category categoryToGet = null;
+
+			if (withPosts)
+			{
+				categoryToGet = await _appDBContext.Categories.Include(category => category.Posts).FirstOrDefaultAsync(category => category.CategoryId == categoryId);
+			}
+			else
+			{
+				categoryToGet = await _appDBContext.Categories.FirstOrDefaultAsync(category => category.CategoryId == categoryId);
+			}
+
+			return categoryToGet;
+		}
+		#endregion
 	}
 }
